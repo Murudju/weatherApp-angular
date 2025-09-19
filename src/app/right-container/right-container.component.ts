b88093c22488db9868e9b62a631265c8ba6344c8
Murudju <stasbond09@gmail.com>
2025-09-19 21:18:27 +0300

    Доработка

--- src/app/right-container/right-container.component.ts
+++ src/app/right-container/right-container.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './right-container.component.css'
 })
 export class RightContainerComponent {
+  today:boolean = false;
+  week:boolean = true;
 
+  celsius:boolean = true;
+  fahrenheit:boolean = false;
+
+  onTodayClick() {
+    this.today = true;
+    this.week = false;
+  }
+
+  onWeekClick() {
+    this.week = true;
+    this.today = false;
+  }
+  onCelsiusClick() {
+    this.celsius = true;
+    this.fahrenheit = false;
+  }
+
+  onFahrenheitClick() {
+    this.fahrenheit = true;
+    this.celsius = false;
+  }
 }
