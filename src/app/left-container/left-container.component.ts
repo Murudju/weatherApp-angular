0d74d01dac55540320454ded1b485e2aa6d5213b
Murudju <stasbond09@gmail.com>
2025-09-24 21:14:19 +0300

    Доработка №3

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faLocationDot, faCloud, faCloudRain } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-left-container',
@@ -9,6 +9,9 @@ import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-ic
   styleUrl: './left-container.component.css'
 })
 export class LeftContainerComponent {
-  faMagnifyingGlass:any = faMagnifyingGlass
-  faLocationDot:any = faLocationDot
+  faMagnifyingGlass:any = faMagnifyingGlass;
+  faLocationDot:any = faLocationDot;
+
+  faCloud:any = faCloud;
+  faCloudRain:any = faCloudRain;
 }
