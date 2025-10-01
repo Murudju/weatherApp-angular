13ed1a9042bd1501260db909612d6625b155eed9
Murudju <stasbond09@gmail.com>
2025-10-01 21:16:03 +0300

    Сделал API

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,15 @@
+export const EnvironmentVariables = {
+    production: false,
+
+    // URL
+    weatherApiLocationBaseURL: 'https://weather338.p.rapidapi.com/locations/search',
+    weatherApiForecastBaseURL: 'https://weather338.p.rapidapi.com/weather/forecast',
+
+    // Api key
+    xRapidApiKeyName: 'x-rapidapi-key',
+    xRapidApiKeyValue: '3faf149c5cmshe78e1fcff590bebp120618jsn7fb1e8689ccd',
+
+    // Api host
+    xRapidApiHostName: 'x-rapidapi-host',
+    xRapidApiHostValue: 'weather338.p.rapidapi.com'
+}
---
+++ src/app/Services/weather.service.ts
@@ -0,0 +1,120 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LocationDetails } from '../Models/LocationDetails';
+import { WeatherDetails } from '../Models/WeatherDetails';
+import { TemperatureData } from '../Models/TemperatureData';
+import { TodayData } from '../Models/TodayData';
+import { WeekData } from '../Models/WeekData';
+import { TodayHightlight } from '../Models/TodaysHightlights';
+import { Observable } from 'rxjs';
+import { get } from 'http';
+import { EnvironmentVariables } from '../Environment/EnvironmentVariables';
+import { response } from 'express';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  cityName:string = "Moscow";
+  language:string = "en-US";
+  date:string = '20251002';
+  units:string = 'm'
+
+  currentTime:Date = new Date();
+  
+  locationDetails?: LocationDetails;
+  weatherDetails?: WeatherDetails;
+
+  temperatureData: TemperatureData;
+  todayData?: TodayData;
+  weekData?: WeekData;
+  todaysHightlights?: TodayHightlight;
+
+  constructor(private httpClient: HttpClient) {
+    this.getData();   
+   }
+
+   getSummaryImage(summary:string):string{
+    var cloudySunny = "https://cdn-icons-png.flaticon.com/128/4814/4814489.png"
+    var rainSunny = "https://cdn-icons-png.flaticon.com/128/3093/3093390.png"
+    var windy = "https://cdn-icons-png.flaticon.com/128/11742/11742622.png"
+    var sunny = "https://cdn-icons-png.flaticon.com/128/2698/2698213.png"
+    var rainy = "https://cdn-icons-png.flaticon.com/128/4735/4735146.png"
+    
+    if(String(summary).includes("Partly Cloudy") || String(summary).includes("P Cloudy") )
+      return cloudySunny
+    else if(String(summary).includes("Partly Rainy") || String(summary).includes("P Rainy") )
+      return rainSunny
+    else if(String(summary).includes("wind"))
+      return windy
+    else if(String(summary).includes("rain"))
+      return rainy
+    else if(String(summary).includes("Sun"))
+      return sunny
+
+
+    return cloudySunny
+   }
+
+   prepareData():void {
+     this.currentTime = new Date();
+     this.temperatureData.day = this.weatherDetails['v3-wx-observations-current'].dayOfWeek;
+     this.temperatureData.time = `${String(this.currentTime.getHours()).padStart(2, '0')}:${String(this.currentTime.getMinutes()).padStart(2, '0')}`
+     this.temperatureData.temperature = this.weatherDetails['v3-wx-observations-current'].temperature;
+     this.temperatureData.location = `${this.locationDetails.location.city[0]},${this.locationDetails.location.country[0]}`
+     this.temperatureData.rainPercent = this.weatherDetails['v3-wx-observations-current'].precip24Hour;
+     this.temperatureData.SummaryPhrase = this.weatherDetails['v3-wx-observations-current'].wxPhraseLong;
+     this.temperatureData.summaryImage = this.getSummaryImage(this.temperatureData.SummaryPhrase)
+   }
+
+  getLocationDetails(cityName: string, language: string):Observable<LocationDetails>{
+    return this.httpClient.get<LocationDetails>(EnvironmentVariables.weatherApiLocationBaseURL, {
+      headers: new HttpHeaders()
+        .set(EnvironmentVariables.xRapidApiKeyName, EnvironmentVariables.xRapidApiKeyValue)
+        .set(EnvironmentVariables.xRapidApiHostName, EnvironmentVariables.xRapidApiHostValue),
+      params: new HttpParams()
+      .set('query', cityName)
+      .set('language', language)
+    })
+  }
+
+  getWeatherReport(date: string, latitude: number, longitude: number, language: string, units: string):Observable<WeatherDetails> {
+    return this.httpClient.get<WeatherDetails>(EnvironmentVariables.weatherApiForecastBaseURL, {
+      headers: new HttpHeaders()
+        .set(EnvironmentVariables.xRapidApiKeyName, EnvironmentVariables.xRapidApiKeyValue)
+        .set(EnvironmentVariables.xRapidApiHostName, EnvironmentVariables.xRapidApiHostValue),
+      params: new HttpParams()
+        .set('date', date)
+        .set('latitude', latitude)
+        .set('longitude', longitude)
+        .set('language', language)
+        .set('units', units)
+    });
+  }
+
+  getData() {
+    let latitude = 0;
+    let longitude = 0;
+    this.getLocationDetails(this.cityName, this.language).subscribe({
+      next:(response)=> {
+        this.locationDetails = response
+        latitude = this.locationDetails?.location.latitude[0];
+        longitude = this.locationDetails?.location.longitude[0];
+
+        this.getWeatherReport(
+          this.date,
+          latitude,
+          longitude,
+          this.language,
+          this.units
+        ).subscribe({
+          next:(response) => {
+            this.weatherDetails = response
+
+            this.prepareData();
+          }
+        })
+      }      
+    })       
+  }
+}
