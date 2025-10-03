0740d3c6c6ddf3adc1e79bdbe1ef4aae9196e225
Murudju <stasbond09@gmail.com>
2025-10-03 21:26:55 +0300

    API

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -18,4 +18,9 @@ export class LeftContainerComponent {
   faCloudRain:any = faCloudRain;
 
   constructor(public weatherService:WeatherService){}
+
+  onSearch (location: string) {
+    this.weatherService.cityName = location;
+    this.weatherService.getData();
+  }
 }
